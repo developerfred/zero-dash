0ebb221ca206d5f83b0a668e15eeb66101151b51
codingsh <codingsh@pm.me>
2024-06-06 15:35:16 +0200

    chore(pages): remove section pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Dashboard from '@/components/Dashboard';
 import Navbar from '@/components/Navbar';
 
 export default function Home() {
-  const [activeSection, setActiveSection] = useState('Zero'); // Define o estado inicial
+  const [activeSection, setActiveSection] = useState('Zero'); 
 
   return (
     <>
