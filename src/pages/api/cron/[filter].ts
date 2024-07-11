626ad2aa42bc10c8262c2cbd7e8ecb3995c9b6e6
codingsh <codingsh@pm.me>
2024-07-11 21:33:22 +0400

    cron(time): acurate time

--- src/pages/api/cron/[filter].ts
+++ src/pages/api/cron/[filter].ts
@@ -24,8 +24,6 @@ interface MetricsData {
     };
 }
 
-const CACHE_DURATION = 15 * 60 * 1000;
-
 const getTokenPrice = async (): Promise<number> => {
     const response = await fetch('https://zero-dash.vercel.app/api/meow/token-price');
     if (!response.ok) {
@@ -35,48 +33,15 @@ const getTokenPrice = async (): Promise<number> => {
     return data.price;
 };
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { filter } = req.query;
-
-    if (filter !== '24h' && filter !== '48h') {
-        return res.status(400).json({ error: 'Invalid filter. Only 24h and 48h are supported.' });
-    }
-
-    const now = Date.now();
-    const fromTs = filter === '24h' ? now - 24 * 60 * 60 * 1000 : now - 48 * 60 * 60 * 1000;
-
-    try {
-        const data = await fetchMetricsDataInChunks(fromTs, now);
-
-        const tokenPrice = await getTokenPrice();
-        const { metricsData, totalRewards, totalMessagesSent, totalDailyActiveUsers, totalUserSignUps } = await addTotalRewardsToData(data, filter, tokenPrice);
-
-        const response = {
-            metricsData,
-            totalRewards,
-            totalMessagesSent,
-            totalDailyActiveUsers,
-            totalUserSignUps
-        };
-
-        await saveDataToSupabase(filter as string, response);
-
-        return res.status(200).json(response);
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return res.status(500).json({ error: (error as Error).message });
-    }
-};
-
 const fetchMetricsDataInChunks = async (fromTs: number, toTs: number): Promise<any[]> => {
-    const interval = 30 * 60 * 1000; 
+    const interval = 30 * 60 * 1000;
     const promises = [];
 
     for (let ts = fromTs; ts < toTs; ts += interval) {
         promises.push(fetchMetricsData(ts, ts + interval));
     }
 
-    const results = await Promise.allSettled(promises); 
+    const results = await Promise.allSettled(promises);
     const fulfilledResults = results
         .filter(result => result.status === 'fulfilled')
         .map(result => (result as PromiseFulfilledResult<any[]>).value);
@@ -84,7 +49,7 @@ const fetchMetricsDataInChunks = async (fromTs: number, toTs: number): Promise<a
 };
 
 const fetchMetricsData = async (fromTs: number, toTs: number): Promise<any[]> => {
-    const response = await fetchWithTimeout(`${API_URL}?fromTs=${fromTs}&toTs=${toTs}`, 10000); 
+    const response = await fetchWithTimeout(`${API_URL}?fromTs=${fromTs}&toTs=${toTs}`, 10000);
     if (!response.ok) {
         throw new Error(`Error fetching data: ${response.statusText}`);
     }
@@ -129,6 +94,28 @@ const normalizeData = (data: any, timestamp: number): any[] => {
     }
 };
 
+const saveDataToSupabase = async (filter: string, data: any) => {
+    if (data.totalDailyActiveUsers > 0 || data.totalMessagesSent > 0 || data.totalUserSignUps > 0 || parseFloat(data.totalRewards.amount) > 0) {
+        const { data: supabaseData, error } = await supabase
+            .from('metrics')
+            .insert([
+                {
+                    filter,
+                    data,
+                    timestamp: new Date().toISOString(),
+                },
+            ]);
+
+        if (error) {
+            console.error('Error saving data to Supabase:', error);
+        } else {
+            console.log('Data saved to Supabase:', supabaseData);
+        }
+    } else {
+        console.log('No valid data to save to Supabase.');
+    }
+};
+
 const addTotalRewardsToData = async (data: any[], filter: string, tokenPrice: number): Promise<{ metricsData: any[], totalRewards: { amount: string, unit: string }, totalMessagesSent: number, totalDailyActiveUsers: number, totalUserSignUps: number }> => {
     if (data.length === 0) return { metricsData: data, totalRewards: { amount: "0.00", unit: "USD" }, totalMessagesSent: 0, totalDailyActiveUsers: 0, totalUserSignUps: 0 };
 
@@ -142,46 +129,78 @@ const addTotalRewardsToData = async (data: any[], filter: string, tokenPrice: nu
     let totalDailyActiveUsers = 0;
     let totalUserSignUps = 0;
 
-    metricsData.forEach(metrics => {
-        totalMessagesSent += metrics.totalMessagesSent;
-        totalDailyActiveUsers += metrics.dailyActiveUsers;
-        totalUserSignUps += metrics.userSignUps;
-    });
-
     if (filter === '24h') {
-        const lastItemMetrics = metricsData[metricsData.length - 1];
-        if (lastItemMetrics && lastItemMetrics.totalRewardsEarned) {
-            const lastRewardInEther = parseFloat(formatUnits(BigInt(lastItemMetrics.totalRewardsEarned.amount), lastItemMetrics.totalRewardsEarned.precision));
+        if (metricsData.length > 0) {
+            const lastMetrics = metricsData[metricsData.length - 1];
+
+            totalMessagesSent = lastMetrics.totalMessagesSent;
+            totalDailyActiveUsers = lastMetrics.dailyActiveUsers;
+            totalUserSignUps = lastMetrics.userSignUps;
+
+            const lastRewardInEther = parseFloat(formatUnits(BigInt(lastMetrics.totalRewardsEarned.amount), lastMetrics.totalRewardsEarned.precision));
             totalRewards.amount = (lastRewardInEther * tokenPrice).toFixed(2);
         }
     } else if (filter === '48h') {
-        const firstItemMetrics = metricsData[0];
-        const lastItemMetrics = metricsData[metricsData.length - 1];
-        if (firstItemMetrics && lastItemMetrics) {
-            const firstRewardInEther = parseFloat(formatUnits(BigInt(firstItemMetrics.totalRewardsEarned.amount), firstItemMetrics.totalRewardsEarned.precision));
-            const lastRewardInEther = parseFloat(formatUnits(BigInt(lastItemMetrics.totalRewardsEarned.amount), lastItemMetrics.totalRewardsEarned.precision));
-            totalRewards.amount = ((firstRewardInEther + lastRewardInEther) * tokenPrice).toFixed(2);
+        let dailyTotals: { [key: string]: MetricsData } = {};
+
+        metricsData.forEach(metrics => {
+            const date = metrics.date;
+            if (!dailyTotals[date]) {
+                dailyTotals[date] = { ...metrics };
+            } else {
+                dailyTotals[date].totalMessagesSent += metrics.totalMessagesSent;
+                dailyTotals[date].dailyActiveUsers += metrics.dailyActiveUsers;
+                dailyTotals[date].userSignUps += metrics.userSignUps;
+                dailyTotals[date].newlyMintedDomains += metrics.newlyMintedDomains;
+                const currentRewardInEther = parseFloat(formatUnits(BigInt(metrics.totalRewardsEarned.amount), metrics.totalRewardsEarned.precision));
+                const totalRewardInEther = parseFloat(formatUnits(BigInt(dailyTotals[date].totalRewardsEarned.amount), dailyTotals[date].totalRewardsEarned.precision));
+                dailyTotals[date].totalRewardsEarned.amount = (currentRewardInEther + totalRewardInEther).toString();
+            }
+        });
+
+        for (const date in dailyTotals) {
+            totalMessagesSent += dailyTotals[date].totalMessagesSent;
+            totalDailyActiveUsers += dailyTotals[date].dailyActiveUsers;
+            totalUserSignUps += dailyTotals[date].userSignUps;
+
+            const rewardInEther = parseFloat(formatUnits(BigInt(dailyTotals[date].totalRewardsEarned.amount), dailyTotals[date].totalRewardsEarned.precision));
+            totalRewards.amount = (parseFloat(totalRewards.amount) + rewardInEther * tokenPrice).toFixed(2);
         }
     }
 
     return { metricsData: data, totalRewards, totalMessagesSent, totalDailyActiveUsers, totalUserSignUps };
 };
 
-const saveDataToSupabase = async (filter: string, data: any) => {
-    const { data: supabaseData, error } = await supabase
-        .from('metrics')
-        .insert([
-            {
-                filter,
-                data,
-                timestamp: new Date().toISOString(),
-            },
-        ]);
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { filter } = req.query;
 
-    if (error) {
-        console.error('Error saving data to Supabase:', error);
-    } else {
-        console.log('Data saved to Supabase:', supabaseData);
+    if (filter !== '24h' && filter !== '48h') {
+        return res.status(400).json({ error: 'Invalid filter. Only 24h and 48h are supported.' });
+    }
+
+    const now = Date.now();
+    const fromTs = filter === '24h' ? now - 24 * 60 * 60 * 1000 : now - 48 * 60 * 60 * 1000;
+
+    try {
+        const data = await fetchMetricsDataInChunks(fromTs, now);
+
+        const tokenPrice = await getTokenPrice();
+        const { metricsData, totalRewards, totalMessagesSent, totalDailyActiveUsers, totalUserSignUps } = await addTotalRewardsToData(data, filter, tokenPrice);
+
+        const response = {
+            metricsData,
+            totalRewards,
+            totalMessagesSent,
+            totalDailyActiveUsers,
+            totalUserSignUps
+        };
+
+        await saveDataToSupabase(filter as string, response);
+
+        return res.status(200).json(response);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return res.status(500).json({ error: (error as Error).message });
     }
 };
 
